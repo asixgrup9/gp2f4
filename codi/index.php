7b9c7bbe5e1545e6cdd6fe76db819e6962f0b234
asixgrup9 <asixgrup9@proton.me>
2022-10-04 17:29:58 +0200

    Operacions

--- codi/index.php
+++ codi/index.php
@@ -1,5 +1,16 @@
-<?php
-    echo "Hola món 2022<br>";
-    echo "Som el grup09";
-    echo "Aquesta és l'activitat GP2F4A6.3"
-?>
+<!DOCTYPE html>
+<html lang="ca">
+	<head>
+		<meta charset="utf-8">
+		<title>
+			PÀGINA INICIAL DE L'APLICACIÓ CALCULADORA
+		</title>
+	</head>
+	<body>
+		<p><b>CALCUALDORA ARITMÈTICA</b></p>
+		<a href="suma.html">a) Suma de 2 operands</a>
+		<a href="resta.html">b) Resta de 2 operands</a>
+		<a href="multiplicacio.html">c) Multiplicació de 2 operands</a>
+		<a href="divisio.html">d)Divisió de 2 operands</a>
+	</body>
+<html>
