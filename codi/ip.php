1cbf9a9201bc949b4cbb3f14e465836ffd227f75
asixgrup9 <asixgrup9@proton.me>
2022-10-07 20:30:43 +0200

    añadido html y php

---
+++ codi/ip.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ca">
+	<head>
+		<meta charset="utf-8">
+		<title>
+			PÀGINA INICIAL DE SUBNET CALCULATOR
+		</title>
+	</head>
+	<body>
+		<p><b>CALCUALDORA DE SUBNET</b></p>
+		<a href="subnet.html">a) CALCULADORA</a>
+	</body>
+<html>
